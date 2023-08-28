a544545ca70d13fe3f2c7ff43741c1527bb432a5
Muhammad Arif Billah <marifbillah524@gmail.com>
2023-08-28 16:27:46 +0700

    first commit

---
+++ src/InputShortner.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const InputShortner = ({ setInputValue }) => {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setInputValue(value);
+    setValue("");
+  };
+
+  return (
+    <div className="inputContainer">
+      <h1>
+        URL <span>Shortner</span>
+        <div>
+          <input
+            type="text"
+            placeholder="Paste a link to shorten it"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button onClick={handleClick}>Shorten It!</button>
+        </div>
+      </h1>
+    </div>
+  );
+};
+
+export default InputShortner;
